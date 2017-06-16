e0bdcb4cbf5569d048a858024ed4ac48b6a905e5
Paul Frazee <pfrazee@gmail.com>
2017-06-16 13:44:09 -0500

    Expose ProgressMonitor alone

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   Archive: require('./model/archive'),
   ArchivesList: require('./model/archives-list'),
   DownloadsList: require('./model/downloads-list'),
-  FileTree: require('./model/file-tree')
+  FileTree: require('./model/file-tree'),
+  ProgressMonitor: require('./model/progress-monitor')
 }
